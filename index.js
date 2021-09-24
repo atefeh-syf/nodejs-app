f194690bff1634f91616424b43518eed6fe26dff
atefeh.syf <atefeh.syf@gmail.com>
2021-09-25 03:20:28 +0330

    feet : changed index.js

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
+process.env.DEBUG = 'app.startup'
+
 const Joi = require('joi');
+const debug = require('debug')('app:startup');
+//const dbDebugger = require('debug')('app:db');
+
+
+const config = require('config');
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
+
+//console.log(`NODE-ENV : ${process.env.NODE_ENV}`);
+//console.log(`app : ${app.get('env')}`);
+
+console.log(`Application name : ${config.get('name')}`);
+console.log(`Mail Server : ${config.get('mail.host')}`);
+//console.log(`Mail Server : ${config.get('mail.password')}`);
+
+if(app.get('env') === 'development'){
+    app.use(morgan('tiny'))
+    //console.log('morgan enabled ...')
+    debug('morgan enabled ...')
+}
+
+//db work ..
+debug('Connected to the database ...');
 
 const genres = [
     { id: 1, name: 'Action' },  
